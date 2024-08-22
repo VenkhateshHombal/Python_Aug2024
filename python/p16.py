1854f5effd959a91133ff010cef13671c3897d1c
VenkhateshHombal <venkhateshhombal@gmail.com>
2024-08-22 17:12:24 +0530

    assignmemnt updadation

--- python/p16.py
+++ python/p16.py
@@ -1,1 +1,9 @@
-#Program to reverse a number
+#Program to Reverse a Number
+input_number = int(input("Enter a number to revers it: "))
+old=input_number
+rev_num = 0
+while input_number>0:
+    temp = input_number%10
+    rev_num = (rev_num*10)+temp
+    input_number=input_number//10
+print("Reverse of a given",old,"is:",int(rev_num))                                                                                                                                                                                                                                                                                       
--- python/p19.py
+++ python/p19.py
@@ -1,6 +1,13 @@
-#Program to print the Right angled Triangle of N lines
-# *
-# **
-# ***
-# ****
-# *****
+'''
+Program to print the Right angled Triangle of N lines
+ *
+ **
+ ***
+ ****
+ *****
+ '''
+input_num=int(input('Enter a number of lines to print right angled triangle'))
+for i in range(input_num+1):
+    print("*"*i)
+    i=i+1
+
