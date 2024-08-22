8329b7566bfef3bd5b8858280515fc86c8daefd1
VenkhateshHombal <venkhateshhombal@gmail.com>
2024-08-22 17:13:57 +0530

    assignment updadation

--- python/p20.py
+++ python/p20.py
@@ -1,6 +1,10 @@
-#Program to print the Equi Lateral Triangle of N lines
-#    *
-#   ***
-#  *****
-# *******
-#*********
+'''
+Program to print the Equi Lateral Triangle of N lines
+    *
+   ***
+  *****
+ *******
+*********
+'''
+input_num=int(input('Enter number of lines tp print that manhy number of equilateral triangle'))
+for i in range(input_num+1):
